343e2f55865bf2ebcc50dc7e4dd8b048e17a5d35
Ben Cohen <162301625+bbencohenn@users.noreply.github.com>
2025-03-23 17:55:49 +1100

    updated to actually work now

--- geoquiz.py
+++ geoquiz.py
@@ -6,11 +6,10 @@ import matplotlib.pyplot as plt
 import pycountry
 from countryinfo import CountryInfo
 
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-
+world = gpd.read_file('ne_110m_admin_0_countries.shp')
 def color_country(country_name):
-    if country_name in world['name'].values:
-        world['color'] = world['name'].map(lambda x: 'red' if x == country_name else 'white')
+    if country_name in world['ADMIN'].values:
+        world['color'] = world['ADMIN'].map(lambda x: 'red' if x == country_name else 'white')
         ax = world.plot(column='color', figsize=(15, 10))
 
         country_info = CountryInfo(country_name)
@@ -22,7 +21,7 @@ def color_country(country_name):
 
         info = f"Country: {country_name}\nCapital: {capital}\nPopulation: {population}\nArea: {area} sq km\nCurrency: {currencies}\nLanguages: {', '.join(languages)}"
 
-        centroid = world.loc[world['name'] == country_name, 'geometry'].centroid.values[0]
+        centroid = world.loc[world['ADMIN'] == country_name, 'geometry'].centroid.values[0]
 
         x_offset = 10
         y_offset = 10
